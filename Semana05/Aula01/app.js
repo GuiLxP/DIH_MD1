00c2fabd4e128e6a92e58576ed97a090d958d82b
GuiExP <guilherme_pantoja@estudante.sesisenai.org.br>
2022-07-25 21:25:07 -0300

    no message

--- Semana05/Aula01/app.js
+++ Semana05/Aula01/app.js
@@ -1,18 +1,49 @@
-class Teste {
-    nome;
-    idade;
-    constructor(nome, idade) {
-        this.nome = nome;
-        this.idade = idade;
-    }
+// const funcaoComumA = () => {
+//     console.log("Sou uma função A!");
+// };
 
-    set idade(idade) {
-        this.idade = idade
-    }
-}
-const romeu = new Teste("Guilherme", 20);
+// const funcaoComumB = () => console.log("Sou uma função B!");
+
+// const quadrado = (n) => {
+//     return n * n;
+// };
+
+// // quando retorna o resultado de uma instrução
+// // não precisa das chaves e do 'return'
+// const multiplica = (a, b) => a * b;
+
+// const vetor = [10, 20, 30];
+
+// const callback = (item) => console.log(item);
+// vetor.forEach(callback);
+
+// vetor.forEach((item) => {
+//     console.log("Item: " + item);
+// });
+
+// vetor.forEach((item) => console.log(item));
+
+// vetor.forEach((item, indice) => console.log(`Item: ${item} - Idx: ${indice}`));
+
+// vetor.forEach((item, indice, array) => {
+//     console.log(`${item} - ${indice}`, array);
+// });
+
+console.log("M1S05-AULA1");
+// Funções equivalentes
+// const batata = function (arroz, feijao) {
+//     console.log(feijao);
+//     console.log(arroz);
+// };
+// batata("Arroz", "Feijão");
+
+// const batata = (feijao, arroz) => {
+//     console.log(feijao, arroz);
+// };
+// batata('feijao', 'arroz');
 
 
 
 
-console.log(romeu);
+
+
